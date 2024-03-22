8189ce089711daffe8d3404608aa05a6c11da132
Natzu2 <amartan16@gmail.com>
2024-03-22 08:12:07 -0700

    fixed typing

--- TestUV.py
+++ TestUV.py
@@ -7,15 +7,15 @@ owm = OWM('25e4f8c6b58ce79d30fb2f9f51c1d1a6')
 uv_mgr = owm.uvindex_manager()
 uv_index = uv_mgr.uvindex_around_coords(27.967115, -110.918797)
 
-def do_something(scheduler): 
-    scheduler.enter(60, 1, do_something, (scheduler,))
+def loop(scheduler): 
+    scheduler.enter(60, 1, loop, (scheduler,))
     print(f"UV Index: {uv_index.value}", f"Exposure Risk: {uv_index.get_exposure_risk()}")
     
 my_scheduler = sched.scheduler(time.time, time.sleep)
-my_scheduler.enter(60, 1, do_something, (my_scheduler,))
+my_scheduler.enter(60, 1, loop, (my_scheduler,))
 my_scheduler.run()
 
 with open('ResultadosUV.csv', 'w', newline='') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter = csv.writer(csvfile, delimiter = ' ', quotechar = '|', quoting=csv.QUOTE_MINIMAL)
     spamwriter.writerow([f'UV Index:{uv_index.value}', f'Exposure Risk: {uv_index.get_exposure_risk()}'])
 
